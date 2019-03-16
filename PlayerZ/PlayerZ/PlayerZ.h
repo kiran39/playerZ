60273bb30da35307ce55ea624744a392a5cc2f2f
Kiran Kumar <Kiran.Kumar@zee.esselgroup.com>
2019-03-16 11:48:29 +0530

    Intial Commit

---
+++ PlayerZ/PlayerZ/PlayerZ.h
@@ -0,0 +1,19 @@
+//
+//  PlayerZ.h
+//  PlayerZ
+//
+//  Created by admin on 16/03/19.
+//  Copyright © 2019 ZEE5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PlayerZ.
+FOUNDATION_EXPORT double PlayerZVersionNumber;
+
+//! Project version string for PlayerZ.
+FOUNDATION_EXPORT const unsigned char PlayerZVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PlayerZ/PublicHeader.h>
+
+
---
+++ PlayerZ/PlayerZ/Service.swift
@@ -0,0 +1,17 @@
+//
+//  Service.swift
+//  PlayerZ
+//
+//  Created by admin on 16/03/19.
+//  Copyright © 2019 ZEE5. All rights reserved.
+//
+
+import Foundation
+public class Service {
+    
+    private init() {}
+    
+    public static func doSomething() -> String {
+        return "Did Something"
+        }
+}
